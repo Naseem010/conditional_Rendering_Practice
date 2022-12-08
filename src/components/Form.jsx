b865d7f047144ee73c19df6d602e6a640dffa187
Faheemuddin Naseem <fnaseem010@gmail.com>
2022-12-08 12:47:34 +0530

    update input and button props

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-function Form() {
+function Form(props) {
   return (
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+     {!props.isRegistered && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
     </form>
   );
 }
